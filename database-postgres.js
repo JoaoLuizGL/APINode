e8cdd4317f2a7fc2676a4eb3c5102f4b4419b1d8
João Luiz Gradwool Lira <128045524+JoaoLuizGL@users.noreply.github.com>
2024-05-17 08:47:48 -0300

    Update database-postgres.js

--- database-postgres.js
+++ database-postgres.js
@@ -3,6 +3,7 @@ import { randomUUID } from "crypto"
 
 export class DataBasePostgres{
 
+    //Método GET para selecionar os vídeos baseados ou não em um 'search'
     async list(search){
         let videos
 
@@ -14,6 +15,7 @@ export class DataBasePostgres{
         return videos
     }
 
+    //Método POST para criar novos registros no banco de dados
     async create(video){
         const videoId = randomUUID()
         const {title, description, duration} = video
@@ -21,12 +23,14 @@ export class DataBasePostgres{
         await sql`insert into videos (id, title, description, duration) VALUES (${videoId}, ${title}, ${description}, ${duration})`
     }
 
+    //Método PUT para atualizar um registro no banco
     async update(id, video) {
         const {title, description, duration} = video
 
         await sql`update videos set title = ${title}, description = ${description}, duration = ${duration} WHERE id = ${id}`
     }
 
+    //Método DELETE para excluir registros do banco
     async delete(id) {
         await sql`delete from videos WHERE id = ${id}`
     }
